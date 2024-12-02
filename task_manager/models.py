5427ca65026a0357847e9daa1f427b2b7aa1e7db
mregorlite <egor.infinity@gmail.com>
2024-12-02 10:33:25 +0200

    make models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("Critical", "Critical"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low")
+    )
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(choices=PRIORITY_CHOICES, max_length=8)
+    task_type = models.ForeignKey(Position, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
