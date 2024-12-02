8d3eb064842b1a3ec08f7026fb3c53e6cca3dd18
mregorlite <egor.infinity@gmail.com>
2024-12-02 19:57:40 +0200

    make create, update, delete, list task type pages

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from task_manager.views import PositionCreateView, PositionListView, PositionUpdateView, PositionDeleteView
+from task_manager.views import PositionCreateView, PositionListView, PositionUpdateView, PositionDeleteView, \
+    TaskTypeListView, TaskTypeCreateView, TaskTypeUpdateView, TaskTypeDeleteView
 
 app_name = "task_manager"
 
 urlpatterns = [
+    path("task-type/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("task-type/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
+    path("task-type/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
+    path("task-type/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
     path("position/", PositionListView.as_view(), name="position-list"),
     path("position/create/", PositionCreateView.as_view(), name="position-create"),
     path("position/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
--- task_manager/views.py
+++ task_manager/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from task_manager.models import Position
+from task_manager.models import Position, TaskType
 
 
 class PositionListView(generic.ListView):
@@ -22,3 +22,27 @@ class PositionUpdateView(generic.UpdateView):
 class PositionDeleteView(generic.DeleteView):
     model = Position
     success_url = reverse_lazy("task_manager:position-list")
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    template_name = "task_manager/task_type_list.html"
+
+
+class TaskTypeCreateView(generic.CreateView):
+    model = TaskType
+    template_name = "task_manager/task_type_form.html"
+    fields = ["name"]
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeUpdateView(generic.UpdateView):
+    model = TaskType
+    template_name = "task_manager/task_type_form.html"
+    fields = ["name"]
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeDeleteView(generic.DeleteView):
+    model = TaskType
+    template_name = "task_manager/task_type_confirm_delete.html"
+    success_url = reverse_lazy("task_manager:task-type-list")
